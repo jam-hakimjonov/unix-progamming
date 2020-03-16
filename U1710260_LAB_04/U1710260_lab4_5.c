d5c506c2843c1c82deea5cae18a4208d0487d3e2
NavaHub <jamshid.khakimjanov@gmail.com>
2020-03-16 23:01:06 +0500

    lab 4 added

--- U1710260_LAB_04/U1710260_lab4_5.c
+++ U1710260_LAB_04/U1710260_lab4_5.c
@@ -17,7 +17,7 @@ int main(int n, char *args[]) {
     struct tm *time_info;
     time(&raw_time);
     time_info = localtime(&raw_time);
-    directory = opendir("./"); // for current root directory 
+    directory = opendir("./"); // for current directory 
     
     if(directory){
         printf("CURRENT LOCAL TIME AND DATE in UNIX EPOCH (SECONDS SINCE 1970.01.01 00:00): %ld", time(time_info));
